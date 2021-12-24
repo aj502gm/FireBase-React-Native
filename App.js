0f311bcbc03a1f0b6d4efe674ceed6bce39eeab8
aj502gm <20004118@galileo.edu>
2021-12-24 10:55:44 -0600

    #3 Firebase dependencies + Provider Wrapper. Grid pending to do

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LogIn from './components/logIn/LogIn';
 import MainMenu from './components/HomeMenu/MainMenu';
+import {AuthProvider} from "./components/Firebase/AuthProvider";
 import {
   SafeAreaView,
   ScrollView,
@@ -31,15 +32,25 @@ const Stack = createNativeStackNavigator();
 const App=  () => {
   
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="LogIn" component={LogIn} options={{ title: 'Welcome Back' }} />
-        <Stack.Screen name="MainMenu" component={MainMenu}  options={{ title: 'Home' }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="LogIn" component={LogIn} options={{ title: 'Welcome Back' }} />
+          <Stack.Screen name="MainMenu" component={MainMenu}  options={{ title: 'Home' }}/>
+         
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   ); 
 };
 
+//<Stack.Screen name="MainMenu" component={MainMenu}  options={{ title: 'Home' }}/>
+        //PANTALLA DE DETALLES PARA LOS PRODUCTOS PENDIENTES DE AGREGAR
+
+
+//INVESTIGACION DE GRID PARA MOSTRAR ELEMENTOS
+        //ES NECESARIO INSTALAR LA DEPENDENCIA GRID LA CUAL PERMITIRÁ CONFIGURAR LAMATRIZ DE DESPLIEGUE
+        //SIN NECESIDAD DE CONFIGURAR POR MEDIO DE CSS
 /*
 
 <NavigationContainer>
